c6a4fe9a347efdce9c2fda4926f471372d695d70
PradhumanSinghNaruka <pradhumannaruka04@gmail.com>
2025-06-04 18:16:31 +0530

    full stack

--- frontend/src/pages/dashboard/Publicprofile.js
+++ frontend/src/pages/dashboard/Publicprofile.js
@@ -18,7 +18,7 @@ const PublicProfile = () => {
     const fetchPublicProfile = async () => {
       try {
         const response = await axios.get(
-          `https://neftap-website-2.onrender.com/userdetail/profile/public/684026c04bf1fccf348c2a98${id}`
+          `https://neftap-website-2.onrender.com/userdetail/profile/public/${id}`
         );
         setUserData(response.data.userdetail);
       } catch (err) {
