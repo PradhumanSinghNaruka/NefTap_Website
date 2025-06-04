8e539716128dd46c9ffc209f7625ca7c128b56bb
PradhumanSinghNaruka <pradhumannaruka04@gmail.com>
2025-06-04 17:41:21 +0530

    full stack

--- frontend/src/pages/dashboard/Slidebar.js
+++ frontend/src/pages/dashboard/Slidebar.js
@@ -177,10 +177,10 @@ export default function UserProfile() {
       </div>
 
       <div className="flex-1 p-4 md:p-8">
-        <div className="block justify-between items-center mb-6 w-[350]">
+        <div className="block justify-between items-center mb-6 w-[350px] md:w-full">
           <h1 className="text-xl md:text-2xl font-bold">Add Your Details</h1>
           {profile && (
-            <p className="text-sm md:text-lg font-thin">
+            <p className="text-sm md:text-lg font-thin w-[350px] md:w-full">
               Public Profile URL: https://neftap-website-2.onrender.com/userdetail/profile/public/{profile._id}
             </p>
           )}
