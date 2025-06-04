b9121ccd96e1dfbcd27a25d85c59946e89897271
PradhumanSinghNaruka <pradhumannaruka04@gmail.com>
2025-06-04 17:38:50 +0530

    full stack

--- frontend/src/pages/dashboard/Slidebar.js
+++ frontend/src/pages/dashboard/Slidebar.js
@@ -177,7 +177,7 @@ export default function UserProfile() {
       </div>
 
       <div className="flex-1 p-4 md:p-8">
-        <div className="block justify-between items-center mb-6">
+        <div className="block justify-between items-center mb-6 w-[350]">
           <h1 className="text-xl md:text-2xl font-bold">Add Your Details</h1>
           {profile && (
             <p className="text-sm md:text-lg font-thin">
