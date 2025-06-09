fa1303f7490d49a6c2b75cff0bed5b76ec8a3656
PradhumanSinghNaruka <pradhumannaruka04@gmail.com>
2025-06-09 15:28:34 +0530

    full stack

--- frontend/src/components/Contactno.js
+++ frontend/src/components/Contactno.js
@@ -20,7 +20,7 @@ function Contactno() {
       message: data.message,
     };
     await axios
-      .post("https://neftap-website-2.onrender.com/contact/contact", userInfo)
+      .post("http://localhost:4001/contact/contact", userInfo)
       .then((res) => {
         console.log(res.data && res.data.message);
         if (res.data) {
