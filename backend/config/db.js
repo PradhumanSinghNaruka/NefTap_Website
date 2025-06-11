4330e539023c2e5116f70e8cf58ff632fe6d16f3
PradhumanSinghNaruka <pradhumannaruka04@gmail.com>
2025-06-11 16:28:01 +0530

    full stack

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,29 @@
-// db.js
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const db = await mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// });
+
+// export default db;
+
+
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = await mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-export default db;
+export default pool;
