0fc651fe65d2c6848d3af5c9fe1e34178df40a00
PradhumanSinghNaruka <pradhumannaruka04@gmail.com>
2025-06-18 16:38:20 +0530

    full stack

--- backend/index.js
+++ backend/index.js
@@ -138,7 +138,12 @@ app.use(
     useTempFiles: true,
   })
 );
-app.use(cors());
+
+app.use(cors({
+  origin: "",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 4001;
