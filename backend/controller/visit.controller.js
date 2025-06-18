fd15491bf9de556cef32a83af381f1a5f2ef049b
PradhumanSinghNaruka <pradhumannaruka04@gmail.com>
2025-06-18 17:58:48 +0530

    full stack

--- backend/controller/visit.controller.js
+++ backend/controller/visit.controller.js
@@ -1,14 +1,12 @@
 import db from "../config/db.js";
 
 export const visit = async (req, res) => {
-  const { userid, source } = req.body;
-
-  if (!userid || !source) {
+  try {
+    const { userid, source, visitCount } = req.body;
+    if (!userid || !source) {
     return res.status(400).json({ message: "userId and source are required" });
   }
-
-  try {
-    await db.execute(
+  await db.execute(
       `INSERT INTO profile_visits (userid, source, visitCount)
        VALUES (?, ?, 1)
        ON DUPLICATE KEY UPDATE 
@@ -18,7 +16,7 @@ export const visit = async (req, res) => {
     );
 
     res.status(200).json({ message: "Visit tracked successfully" });
-  } catch (err) {
+  }catch (err) {
     res.status(500).json({
       message: "Error tracking visit",
       error: err.message,
